4801ec1a0fb7e3cf725a7c7f9729a90be416f732
Gláucia Silva Bierwagen <98116061+glauciabierwagen@users.noreply.github.com>
2026-02-24 15:24:31 +0100

    Gitignore

--- dist/cli.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-//Menu loop, code given by instructions for the CLI interface, and handling user input. 
-// This is where the user will interact with the application and make choices about what they want to do.
-// cli.ts
